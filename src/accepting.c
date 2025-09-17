53c707804f66c2ba309f5e732f1319433d1160ee
aiden-aquino <aaquino510@gmail.com>
2025-09-17 13:23:58 -0400

    Implemented accepting state files (UNTESTED)

---
+++ src/accepting.c
@@ -0,0 +1,31 @@
+/* Assignment: PA-01 FSM
+ * Date: 09/17/2025
+ *
+ * Team #: FSM-PA Team 13
+ * Authors : Aiden Aquino, William Hudson
+ * File Name: accepting.c
+ */
+#include "accepting.h"
+#include "system.h"
+#include "statemodel.h"
+
+extern state_t* default_event_handler();
+extern void default_action();
+
+ state_t accepting  = {
+    order_received, // Order_recieved
+    default_event_handler, // Invalid Payment
+    default_event_handler, // Valid Payment
+    default_event_handler, // Manufacture failed
+    default_event_handler, // Manufacture completed
+    default_event_handler, // Shipment lost
+    default_event_handler, // Shipment arrived
+    default_action, // Entry
+    default_action // Exit
+ };
+
+state_t* order_received()
+{
+    getOrderSize();
+    resetAttempts();
+}
---
+++ src/accepting.h
@@ -0,0 +1,17 @@
+/* Assignment: PA-01 FSM
+ * Date: 09/17/2025
+ *
+ * Team #: FSM-PA Team 13
+ * Authors : Aiden Aquino, William Hudson
+ * File Name: accepting.h
+ */
+
+ #ifndef accepting_h
+ #define accepting_h
+
+
+ #include "state.h"
+
+ static state_t* order_received();
+
+ #endif
